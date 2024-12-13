485fb06af7a090660d2e50dbbce5ea59a692b46e
mehran feriduni <m3hr4nus@gmail.com>
2024-12-13 11:34:23 +0330

    create user schema

--- src/user.module.ts
+++ src/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import {  UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./schema/user.schema";
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/microservice-todo')
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/microservice-todo'),
+    MongooseModule.forFeature([{name:User.name,schema:UserSchema}])
   ],
   controllers: [UserController],
   providers: [UserService],
